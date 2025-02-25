88f9ed725064fc3f338928c12da8c5af2e3fefe3
smitesh-cfx <121931356+smitesh-cfx@users.noreply.github.com>
2025-02-25 13:34:40 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -7,5 +7,4 @@ npx redoc-cli bundle openapi.json && \
 mv redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
-sed -i '7 i \ \ <link rel="icon" type="image/png" href="images/favicon.png"/>' index.html && \
 echo -e "\nDone!"
